3fc67d5fcb3afc8250ca61771dabe67f14e9697f
Martin Galpin <galpin@gmail.com>
2022-08-09 19:01:25 +0100

    Add poetry-nox and re-enable nox tests.

--- noxfile.py
+++ noxfile.py
@@ -1,24 +1,18 @@
-import nox
+from nox_poetry import session
 
-versions = ['3.9', '3.9', '3.10']
+versions = ['3.9', '3.10']
 
 
-@nox.session(python=versions)
+@session(python=versions)
 def tests_json(session):
-    install_requirements(session)
     pytest(session)
 
 
-@nox.session(python=versions)
+@session(python=versions)
 def tests_orjson(session):
-    install_requirements(session)
     session.install('orjson')
     pytest(session)
 
 
 def pytest(session):
     session.run('pytest')
-
-
-def install_requirements(session):
-    session.install('-r', 'requirements.txt')
